38b1a993bbad4a86e11c406dd8a256198889c1a1
elmi.a <shahrokhelmi@gmail.com>
2023-06-02 19:07:24 +0330

    ch03-01 completed'

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    // https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html
+    // ch03-01
+
+    // let mut x = 5;
+    // println!("The value x is {x}");
+    // x = 6;
+    // println!("The value x is {x}");
+
+    // let space = "    ";
+    // let space = space.len();
+    // println!("space is: {space}");        
+
+    // let mut space = "   ";
+    // space = space.len();
+    // println!("space is : {space}");
+
+    // let x = 6;
+    // println!("the x is: {x}");
+    // let x = x + 2;
+    // println!("the x is: {x}");
+    // {
+    //     let x = x +10;
+    //     println!("in new scope: the x is: {x}");
+    // }
+    // println!("after scope: the x is: {x}");
+
+    // ch03-02
+    // https://doc.rust-lang.org/book/ch03-02-data-types.html
+
+    // Data types
+
+    let guess = "42".parse().expect("Not a number");
+    println!("the guess is: {guess}")
+
+    // Scaler types
+    // Integer type
+
+}
